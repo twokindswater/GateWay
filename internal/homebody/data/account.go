ab8d22738e6c7d69a95216b898474720f9003f6b
twokindswater <jongsoo.lee@samsung.com>
2021-07-07 00:33:39 +0900

    Change account struct info

--- internal/homebody/data/account.go
+++ internal/homebody/data/account.go
@@ -1,15 +1,12 @@
 package data
 
 type AccountInfo struct {
-	Id       string   `json:"id"`
-	Image    string   `json:"image"`
-	SSID     string   `json:"ssid"`
-	BSSID    string   `json:"bssid"`
-	TimeInfo TimeInfo `json:"timeInfo"`
-}
-
-type TimeInfo struct {
-	Total int
-	Week  int
-	Day   int
+	Id        string  `json:"id"`
+	Name      string  `json:"name"`
+	Image     string  `json:"image"`
+	SSID      string  `json:"ssid"`
+	BSSID     string  `json:"bssid"`
+	Street    string  `json:"street"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
